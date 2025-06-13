5375a262d39930ebc5fd07fba6a60872ffc54e1e
sanket1381 <sanket.k@ergobite.com>
2025-06-13 13:47:07 +0000

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ['admin.simplifin.in'],
+    host: true, // allow external access
+    port: 5173 // optional: if you want to fix the port
+  }
+});
